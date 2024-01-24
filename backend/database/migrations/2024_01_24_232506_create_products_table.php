06ec54e14be3b981a944ae383298a81ff94620a8
xfunktastic <ignacio.avendano@alumnos.ucn.cl>
2024-01-24 20:47:20 -0300

    feat: Product migrations

--- backend/database/migrations/2024_01_24_232506_create_products_table.php
+++ backend/database/migrations/2024_01_24_232506_create_products_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->string('image');
         });
     }
 
