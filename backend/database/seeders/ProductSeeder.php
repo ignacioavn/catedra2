3eda8cef3b6438d271626bd4ee6605c0df06bccd
xfunktastic <ignacio.avendano@alumnos.ucn.cl>
2024-01-24 21:52:29 -0300

    fix: Product Seeder

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -12,6 +12,66 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        \App\Models\Product::create([
+            'name' => 'Comino Molido',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '5000',
+            'image' => 'images/1.jfif',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Castañas de caju',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '3000',
+            'image' => 'images/2.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Ciboulette',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '2500',
+            'image' => 'images/3.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Splenda Stevia',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '6000',
+            'image' => 'images/4.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Mix Italiano',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '2500',
+            'image' => 'images/5.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Nueces',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '1500',
+            'image' => 'images/6.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Papas',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '4000',
+            'image' => 'images/7.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Espinaca',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '5000',
+            'image' => 'images/8.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Huevos',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '7000',
+            'image' => 'images/9.jpg',
+        ]);
+        \App\Models\Product::create([
+            'name' => 'Soda Caústica',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit neque maecenas, turpis ornare donec litora quisque est blandit. Senectus hendrerit phasellus torquent nostra tempus nisl fames semper, est et ph.',
+            'price' => '2600',
+            'image' => 'images/10.jpg',
+        ]);
     }
 }
+
